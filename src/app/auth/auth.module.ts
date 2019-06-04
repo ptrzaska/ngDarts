cc2b5d5e2b6e9c028a755d76bc09ff3db28fe84d
przemek.trzaska@gmail.com <przemek.trzaska@gmail.com>
2019-06-04 20:50:01 +0200

    AngularFire Modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from "@angular/core";
-import { SignupComponent } from "./signup/signup.component";
-import { LoginComponent } from "./login/login.component";
-import { MaterialModule } from "../material.module";
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
